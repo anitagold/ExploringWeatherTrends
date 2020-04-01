efe129d10c1ead5c29fc93e0793075bb126c33d3
Anita G. Goldpergel <anita.goldpergel@gmail.com>
2020-04-01 03:12:51 +0200

    another cities data downloaded (Boston, Berlin, Barcelona), data directory added, sql script added

---
+++ download_data.sql
@@ -0,0 +1,21 @@
+# lets check which cities avavalable from Hungary
+SELECT * FROM city_list WHERE country = 'Hungary'
+
+# get Budapest data
+SELECT * FROM city_data WHERE country = 'Hungary' AND city = 'Budapest'
+
+# get global data
+SELECT * FROM global_data
+
+# get another city data
+# check cities and countries first
+SELECT * FROM city_list
+
+# get Boston data
+SELECT * FROM city_data WHERE country = 'United States' AND city = 'Boston'
+
+# get Berlin data
+SELECT * FROM city_data WHERE country = 'Germany' AND city = 'Berlin'
+
+# get Barcelona data
+SELECT * FROM city_data WHERE country = 'Spain' AND city = 'Barcelona'
